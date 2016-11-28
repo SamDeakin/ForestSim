129e3e6cbe01f8ec31e55600d3756058638478e6
Sam Deakin <sam_d1995@hotmail.com>
2016-11-28 18:04:00 -0500

    Initial Commit - Stuff should build and link but theres nothing there

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <OpenGL/glext.h>
+#include <GLFW/glfw3.h>
+#include <lua.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include "OpenGLImport.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    glm::vec3 v = glm::vec3();
+    return 0;
+}
